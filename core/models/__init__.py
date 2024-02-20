ec5c50f3863bc36e0bc65755301fe00ce0a177f9
NickyBricky <buninkola16@gmail.com>
2024-02-20 12:49:52 +0300

    create alembic

---
+++ core/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = (
+    "Base",
+    "User",
+    "Databasehelper",
+    "db_helper"
+)
+
+
+from .base import Base
+from .db_helper import Databasehelper, db_helper
+from .user import User
