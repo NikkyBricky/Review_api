af366ae38407977ff814e0c37965cfa78cf7d8ea
NickyBricky <buninkola16@gmail.com>
2024-02-21 11:55:59 +0300

    fix bugs and add delete project func

--- api_v1/project/crud.py
+++ api_v1/project/crud.py
@@ -1,9 +1,6 @@
-from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Path
 from .schemas import ProjectCreate
 from core.models import Project
-from typing import Annotated
 
 
 async def create_project(
@@ -15,20 +12,6 @@ async def create_project(
     await session.commit()
     await session.refresh(project)
     return project
-
-
-async def get_project_by_project_difficulty(
-        session: AsyncSession,
-        project_difficulty: Annotated[int, Path(ge=1, le=10)]
-) -> list[Project]:
-    stmt = (
-        select(Project)
-        .where(Project.project_difficulty == project_difficulty)
-        .order_by(Project.user_id)
-    )
-    result: Result = await session.execute(stmt)
-    projects = result.scalars().all()
-    return list(projects)
 
 
 async def delete_project(
