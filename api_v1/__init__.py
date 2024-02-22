42b3f0fd4ae249507e6fd2e07100eff1b1256c04
NickyBricky <buninkola16@gmail.com>
2024-02-22 18:57:11 +0300

    create logic for reviews

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .user.views import router as user_router
 from .project.views import router as project_router
+from .review.views import router as review_router
 router = APIRouter()
 router.include_router(router=user_router, prefix="/users")
 router.include_router(router=project_router, prefix="/projects")
+router.include_router(router=review_router, prefix="/reviews")
