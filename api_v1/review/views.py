6c20bffe91aafea0fd2ac256211d90cbd597e121
NickyBricky <buninkola16@gmail.com>
2024-02-23 12:22:05 +0300

    move funcs for getting info into dependencies

--- api_v1/review/views.py
+++ api_v1/review/views.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .schemas import ReviewSend, ReviewSchema
 from core.models import db_helper
-from .crud import get_review_by_user_id
+from .dependencies import get_review_by_user_id
 router = APIRouter(tags=["Reviews"])
 
 
@@ -57,9 +57,20 @@ async def delete_review_text(
         user_id: int,
         session: AsyncSession = Depends(db_helper.session_dependency)
 ):
+
     review_schema = ReviewSchema(user_id=user_id, review_text="no review")
     review = await get_review_by_user_id(session=session, user_id=user_id)
+
+    if review.review_text == "no review":
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"message": f"user with user_id {user_id} does not have a review yet"}
+        )
+
     for review_data, value in review_schema.model_dump(exclude_none=True).items():
         setattr(review, review_data, value)
+
     await session.commit()
+
     return {"message": f"Successfully deleted review from user with user_id {user_id}"}
