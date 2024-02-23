81fb951a336f0d9edcd84a4235e18fd5ca17ad57
NickyBricky <buninkola16@gmail.com>
2024-02-23 12:22:30 +0300

    move funcs for getting info into dependencies

---
+++ api_v1/user/dependencies.py
@@ -0,0 +1,45 @@
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Project, Review, User
+
+
+async def get_project_by_user_id(
+        session: AsyncSession,
+        user_id: int
+):
+
+    project = await session.get(Project, user_id)
+
+    if project:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"message": f"cannot delete user with user_id {user_id} while he has got a project in database"}
+        )
+
+
+async def get_review_by_user_id(
+        session: AsyncSession,
+        user_id: int
+):
+
+    review = await session.get(Review, user_id)
+
+    if review:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"message": f"cannot delete user with user_id {user_id} while he has got a review in database"}
+        )
+
+
+async def get_user_by_user_id(
+        session: AsyncSession,
+        user_id: int
+):
+    user = await session.get(User, user_id)
+    if user:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail={"message": f"user with user_id {user_id} is unauthorised"}
+    )
