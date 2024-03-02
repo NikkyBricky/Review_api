03f4acc92413390297d477a093bf38186e262227
NickyBricky <buninkola16@gmail.com>
2024-03-03 00:56:17 +0300

    change env files

--- core/config.py
+++ core/config.py
@@ -6,11 +6,11 @@ import os
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 
 class DbSettings(BaseModel):
