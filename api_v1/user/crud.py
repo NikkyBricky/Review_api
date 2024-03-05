c6fc357e9c0bf78b49378f5f93c5f564a96522e7
NickyBricky <buninkola16@gmail.com>
2024-03-05 17:17:32 +0300

    move hashing password func to auth

--- api_v1/user/crud.py
+++ api_v1/user/crud.py
@@ -1,7 +1,8 @@
-import bcrypt
 import sqlalchemy
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException, status
+
+from .auth import check_password, hash_password
 from .schemas import UserCreate, UserSchema
 from core.models import User
 from .dependencies import get_user_by_user_id, get_review_by_user_id
@@ -13,9 +14,9 @@ async def create_user(
 ):
     password = user_in.password
     user_id = user_in.user_id
-    salt = bcrypt.gensalt()
-    pwd_bytes = password.encode()
-    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+
+    hashed_password = hash_password(password=password)
+
     user = User(user_id=user_id, password=hashed_password)
 
     session.add(user)
@@ -34,24 +35,17 @@ async def log_in_user(
         session: AsyncSession,
         user_in: UserSchema
 ) -> bool:
-    password = UserSchema(**user_in.model_dump()).password
-    user_id = UserSchema(**user_in.model_dump()).user_id
+    password = user_in.password
+    user_id = user_in.user_id
     user = await get_user_by_user_id(
         session=session,
         user_id=user_id
     )
 
     correct_hashed_password = user.password
-    result = bcrypt.checkpw(
-        password=password.encode(),
-        hashed_password=correct_hashed_password,
-    )
-    if result:
-        return result
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail={"message": "password is incorrect"}
-    )
+    result = check_password(correct_hashed_password=correct_hashed_password,
+                            password=password)
+    return result
 
 
 async def delete_user(
