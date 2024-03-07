e12c7cca15aef9a79ff831cebd83a950efdbfd20
NickyBricky <buninkola16@gmail.com>
2024-03-07 15:03:13 +0300

    change review schemas in a more correct way

--- api_v1/review/schemas.py
+++ api_v1/review/schemas.py
@@ -4,20 +4,16 @@ from annotated_types import MinLen, MaxLen
 
 
 class ReviewBase(BaseModel):
-    review_text: Annotated[str, MinLen(70), MaxLen(2000)]
-    review_id: int
     user_id: int
 
 
 class ReviewCreate(ReviewBase):
-    review_text: None = None
+    review_id: int
 
 
 class ReviewSend(ReviewBase):
-    review_id: None = None
+    review_text: Annotated[str, MinLen(70), MaxLen(2000)]
 
 
-class ReviewSchema(ReviewBase):
+class ReviewDelete(ReviewBase):
     review_text: str = "no review"
-    review_id: None = None
-    user_id: int
