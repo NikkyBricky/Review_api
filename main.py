734be79a8e474d7da800b978db359111d94c2d69
NickyBricky <buninkola16@gmail.com>
2024-03-13 16:47:33 +0300

    move title, summary and version of api to api_description file

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from api_v1 import router as router_v1
 from core.config import settings
-from api_description import description
-
+from api_description import description, summary, version, title
 
 app = FastAPI(
-    title="Review API",
+    title=title,
     description=description,
-    summary="ReviewApp finds a pair of users who have projects with the same project difficulty."
-            " Then it allows this users to review their work and send the results to each other. ",
-    version="0.0.1")
+    summary=summary,
+    version=version)
 
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
