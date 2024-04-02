19adbd0864c06dd115cab78874df2535e0ea8cf7
NickyBricky <buninkola16@gmail.com>
2024-04-02 16:45:11 +0300

    make letters uppercase

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from fastapi import FastAPI
 from api_v1 import router as router_v1
 from core.config import settings
-from api_description import description, summary, version, title
+from api_description import TITLE, DESCRIPTION, SUMMARY, VERSION
 
 app = FastAPI(
-    # TODO Как будто бы эти переменные тоже можно сделать большими буквами 
-    title=title,
-    description=description,
-    summary=summary,
-    version=version)
+    title=TITLE,
+    description=DESCRIPTION,
+    summary=SUMMARY,
+    version=VERSION)
 
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
-
-# TODO Ну вот это явно стоит удалить в деплойнутой версии
-@app.get("/")
-def start_message():
-    return {"message": "Hi there! This is an api for reviewing projects."}
