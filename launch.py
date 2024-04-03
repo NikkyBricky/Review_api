b9492997384afa2679002ba22f40f09375014877
NickyBricky <buninkola16@gmail.com>
2024-04-03 16:54:00 +0300

    starting bot in launch

--- launch.py
+++ launch.py
@@ -1,14 +1,6 @@
 import uvicorn
 from api_bot.bot import run_bot
 from multiprocessing import Process
-import logging
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H"
-)
 
 
 def start_api():
@@ -18,8 +10,6 @@ def start_api():
 if __name__ == "__main__":
     api = Process(target=start_api)
     api.start()
-    logging.info("Апи запущено")
 
-    api_bot = Process(target=run_bot)
-    api_bot.start()
-    logging.info("Бот запущен")
+    # api_bot = Process(target=run_bot)
+    # api_bot.start()
