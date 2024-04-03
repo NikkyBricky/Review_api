2f8dc934abe0631553fc57ab47b77fc7f20f5371
NickyBricky <buninkola16@gmail.com>
2024-04-03 14:51:12 +0300

    starting bot in launch

--- launch.py
+++ launch.py
@@ -1,5 +1,19 @@
 import uvicorn
+from api_bot.bot import bot
+from multiprocessing import Process
+
+
+def start_bot():
+    bot.polling(none_stop=True)
+
+
+def start_api():
+    uvicorn.run("main:app", reload=True, port=8000)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=8000)
+    api = Process(target=start_api)
+    api.start()
+    bot = Process(target=start_bot)
+    bot.start()
 
