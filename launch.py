33f7b7a148c50551b5f1fceee7eb2d0feacd3b67
NickyBricky <buninkola16@gmail.com>
2024-04-03 15:16:38 +0300

    starting bot in launch

--- launch.py
+++ launch.py
@@ -4,6 +4,14 @@ from multiprocessing import Process
 import logging
 
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H",
+    filemode="w",
+    force=True
+)
+
 def start_bot():
     bot.polling(none_stop=True)
 
@@ -16,7 +24,8 @@ if __name__ == "__main__":
     api = Process(target=start_api)
     api.start()
     logging.info("Апи запущено")
+    print("Апи запущено")
     api_bot = Process(target=start_bot)
     api_bot.start()
     logging.info("Бот запущен")
-
+    print("Бот запущен")
